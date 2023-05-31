fe070de79ff640e0d5b1ed4d0e107e872ae16d3b
Likith Shet <96128274+shet27@users.noreply.github.com>
2023-05-31 20:38:27 +0530

    Updated Search using linear and binary.java
    Added code to find execution time

--- Search using linear and binary.java
+++ Search using linear and binary.java
@@ -1,45 +1,93 @@
-//Program to assign 100 values and search using binary and linear
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Random;
+import java.util.Scanner;
 
-public class SearchArrayList {
-    public static void main(String[] args) {
-        ArrayList<Double> list = new ArrayList<>();
-        Random rand = new Random();
+public class Search {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ArrayList<Integer> numbers = new ArrayList<>();
+        Random random = new Random();
+        Scanner SC = new Scanner(System.in);
+        
+        long startTime1,startTime2,endTime1,endTime2,executionTime1,executionTime2;
+
+        // Generate 100 random double values and add them to the ArrayList
         for (int i = 0; i < 100; i++) {
-            list.add(rand.nextDouble());
+        	numbers.add(random.nextInt(100));
         }
-        double searchValue = 0.5;
-        int linearIndex = linearSearch(list, searchValue);
-        System.out.println("Linear search: " + linearIndex);
-        Collections.sort(list);
-        int binaryIndex = binarySearch(list, searchValue);
-        System.out.println("Binary search: " + binaryIndex);
-    }
+        Collections.shuffle(Arrays.asList(numbers));
 
-    public static int linearSearch(ArrayList<Double> list, double value) {
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i) == value) {
+        // Sort the ArrayList for binary search
+        Collections.sort(numbers);
+        
+        System.out.println(numbers);
+
+        // Perform a linear search
+        startTime1 = System.nanoTime();
+        System.out.print("Enter the element you want to find:");
+        int searchNumber =SC.nextInt(); // Number to search
+        int linearSearchIndex = linearSearch(numbers, searchNumber);
+        endTime1 = System.nanoTime();
+        if (linearSearchIndex != -1) {
+            System.out.println("Linear search: Found " + searchNumber + " at index " + linearSearchIndex);
+        } else {
+            System.out.println("Linear search: " + searchNumber + " not found");
+        }
+
+        // Perform a binary search
+    	startTime2 = System.nanoTime();
+        int binarySearchIndex = binarySearch(numbers, searchNumber);
+        endTime2 = System.nanoTime();
+
+        if (binarySearchIndex != -1) {
+            System.out.println("Binary search: Found " + searchNumber + " at index " + binarySearchIndex);
+        } else {
+            System.out.println("Binary search: " + searchNumber + " not found");
+        }
+        
+        executionTime1 = (endTime1 - startTime1);
+        System.out.println("Execution time for linear search : " + executionTime1 + " s");
+        
+        executionTime2 = (endTime2 - startTime2);
+        System.out.println("Execution time for binary search : " + executionTime2 + " s");
+        
+	}
+
+    // Linear search implementation
+	
+    private static int linearSearch(ArrayList<Integer> numbers, int searchNumber) {
+    	
+        for (int i = 0; i < numbers.size(); i++) {
+            if (numbers.get(i) == searchNumber) {
                 return i;
             }
         }
-        return -1;
+        return -1; // Number not found       
     }
+    // Binary search implementation
+     private static int binarySearch(ArrayList<Integer> numbers, int searchNumber) {
+        int left = 0;
+        int right = numbers.size() - 1;
 
-    public static int binarySearch(ArrayList<Double> list, double value) {
-        int low = 0;
-        int high = list.size() - 1;
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            if (list.get(mid) == value) {
-                return mid;
-            } else if (list.get(mid) < value) {
-                low = mid + 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (numbers.get(mid) == searchNumber) {
+                return mid; // Number found
+            }
+
+            if (numbers.get(mid) < searchNumber) {
+                left = mid + 1; // Search the right half
             } else {
-                high = mid - 1;
+                right = mid - 1; // Search the left half
             }
         }
-        return -1;
-    }
+
+        return -1; // Number not found
+	}
+    
 }
