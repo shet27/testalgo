80686abeb29d4e7d875dfc9a81f701a47dab3846
Likith Shet <96128274+shet27@users.noreply.github.com>
2023-06-01 17:28:38 +0530

    Update Search using linear and binary.java

--- Search using linear and binary.java
+++ Search using linear and binary.java
@@ -1,36 +1,42 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
+package org.example;
+
+import java.util.HashSet;
 import java.util.Random;
 import java.util.Scanner;
-
+import java.util.ArrayList;
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		ArrayList<Integer> numbers = new ArrayList<>();
+    public static void main(String[] args) {
+        HashSet<Integer> numbers = new HashSet<>();
         Random random = new Random();
-        Scanner SC = new Scanner(System.in);
-        
-        long startTime1,startTime2,endTime1,endTime2,executionTime1,executionTime2;
+        Scanner scanner = new Scanner(System.in);
 
-        // Generate 100 random double values and add them to the ArrayList
-        for (int i = 0; i < 100; i++) {
-        	numbers.add(random.nextInt(100));
+        long startTime1, startTime2, endTime1, endTime2, executionTime1, executionTime2;
+
+        int count = 100; // Number of random numbers to generate
+        int min = 1; // Minimum random number
+        int max = 1000; // Maximum random number
+
+        // Generate random numbers until the set has the desired count
+        while (numbers.size() < count) {
+            int randomNumber = random.nextInt(max - min + 1) + min;
+            numbers.add(randomNumber);
         }
-        Collections.shuffle(Arrays.asList(numbers));
 
-        // Sort the ArrayList for binary search
-        Collections.sort(numbers);
-        
+        // Print the generated numbers
         System.out.println(numbers);
+
+        // Convert the set to an array
+        Integer[] numbersArray = numbers.toArray(new Integer[0]);
+
+        // Sort the array for binary search
+        java.util.Arrays.sort(numbersArray);
 
         // Perform a linear search
         startTime1 = System.nanoTime();
-        System.out.print("Enter the element you want to find:");
-        int searchNumber =SC.nextInt(); // Number to search
-        int linearSearchIndex = linearSearch(numbers, searchNumber);
+        System.out.print("Enter the element you want to find: ");
+        int searchNumber = scanner.nextInt(); // Number to search
+        int linearSearchIndex = linearSearch(numbersArray, searchNumber);
         endTime1 = System.nanoTime();
         if (linearSearchIndex != -1) {
             System.out.println("Linear search: Found " + searchNumber + " at index " + linearSearchIndex);
@@ -39,8 +45,8 @@ public class Main {
         }
 
         // Perform a binary search
-    	startTime2 = System.nanoTime();
-        int binarySearchIndex = binarySearch(numbers, searchNumber);
+        startTime2 = System.nanoTime();
+        int binarySearchIndex = binarySearch(numbersArray, searchNumber);
         endTime2 = System.nanoTime();
 
         if (binarySearchIndex != -1) {
@@ -48,46 +54,43 @@ public class Main {
         } else {
             System.out.println("Binary search: " + searchNumber + " not found");
         }
-        
-        executionTime1 = (endTime1 - startTime1);
-        System.out.println("Execution time for linear search : " + executionTime1 + " s");
-        
-        executionTime2 = (endTime2 - startTime2);
-        System.out.println("Execution time for binary search : " + executionTime2 + " s");
-        
-	}
+
+        executionTime1 = endTime1 - startTime1;
+        System.out.println("Execution time for linear search: " + executionTime1 + " ns");
+
+        executionTime2 = endTime2 - startTime2;
+        System.out.println("Execution time for binary search: " + executionTime2 + " ns");
+    }
 
     // Linear search implementation
-	
-    private static int linearSearch(ArrayList<Integer> numbers, int searchNumber) {
-    	
-        for (int i = 0; i < numbers.size(); i++) {
-            if (numbers.get(i) == searchNumber) {
+    private static int linearSearch(Integer[] numbers, int searchNumber) {
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] == searchNumber) {
                 return i;
             }
         }
-        return -1; // Number not found       
+        return -1; // Number not found
     }
+
     // Binary search implementation
-     private static int binarySearch(ArrayList<Integer> numbers, int searchNumber) {
+    private static int binarySearch(Integer[] numbers, int searchNumber) {
         int left = 0;
-        int right = numbers.size() - 1;
+        int right = numbers.length - 1;
 
         while (left <= right) {
             int mid = left + (right - left) / 2;
 
-            if (numbers.get(mid) == searchNumber) {
+            if (numbers[mid] == searchNumber) {
                 return mid; // Number found
             }
 
-            if (numbers.get(mid) < searchNumber) {
+            if (numbers[mid] < searchNumber) {
                 left = mid + 1; // Search the right half
             } else {
                 right = mid - 1; // Search the left half
             }
         }
 
-        return -1; // Number not found
-	}
-    
+        return -1; // Number not found
+    }
 }
